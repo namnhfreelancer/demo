ece3a8a426e19ec2ae4e7e1910dbf4a3005d0787
ナム <namnhfreelancer@gmail.com>
2024-02-21 23:28:59 +0700

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -32,7 +32,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # Speedtest
 docker run --rm robinmanuelthiel/speedtest:latest
 sudo chmod +x /etc/profile.d/proxy.sh
