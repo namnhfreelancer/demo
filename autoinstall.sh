aabadb96773287644a917c8dc99278204b63e362
ナム <namnhfreelancer@gmail.com>
2024-02-22 10:01:23 +0700

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -38,26 +38,26 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 sudo docker run --rm robinmanuelthiel/speedtest:latest
 
 #Proxy
-read -p "Enter proxy IP: " proxy_ip
-read -p "Enter proxy port: " proxy_port
-read -p "Enter proxy user: " proxy_user
-read -sp "Enter proxy password: " proxy_pass
-echo
+# read -p "Enter proxy IP: " proxy_ip
+# read -p "Enter proxy port: " proxy_port
+# read -p "Enter proxy user: " proxy_user
+# read -sp "Enter proxy password: " proxy_pass
+# echo
 
-export http_proxy="http://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
-export https_proxy="https://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
-export ftp_proxy="ftp://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
-export no_proxy="localhost,127.0.0.1,::1"
+# export http_proxy="http://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
+# export https_proxy="https://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
+# export ftp_proxy="ftp://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/"
+# export no_proxy="localhost,127.0.0.1,::1"
 
-echo "http_proxy=\"http://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
-echo "https_proxy=\"https://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
-echo "ftp_proxy=\"ftp://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
-echo "no_proxy=\"localhost,127.0.0.1,::1\"" | sudo tee -a /etc/environment
+# echo "http_proxy=\"http://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
+# echo "https_proxy=\"https://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
+# echo "ftp_proxy=\"ftp://$proxy_user:$proxy_pass@$proxy_ip:$proxy_port/\"" | sudo tee -a /etc/environment
+# echo "no_proxy=\"localhost,127.0.0.1,::1\"" | sudo tee -a /etc/environment
 
-echo "Proxy settings configured."
-curl ipinfo.io/ip
+# echo "Proxy settings configured."
+# curl ipinfo.io/ip
 # Speedtest after proxy
-sudo docker run --rm robinmanuelthiel/speedtest:latest
+# sudo docker run --rm robinmanuelthiel/speedtest:latest
 # Io.net
 curl -L https://github.com/ionet-official/io-net-official-setup-script/raw/main/ionet-setup.sh -o ionet-setup.sh
 chmod +x ionet-setup.sh && ./ionet-setup.sh
