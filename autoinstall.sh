c362911def0d77ddb99a1974a3cb84788f2d152f
ナム <namnhfreelancer@gmail.com>
2024-02-25 16:00:15 +0700

    fix ionet

--- autoinstall.sh
+++ autoinstall.sh
@@ -139,4 +139,26 @@ curl -L https://github.com/ionet-official/io-net-official-setup-script/raw/main/
 chmod +x ionet-setup.sh && ./ionet-setup.sh
 curl -L https://github.com/ionet-official/io_launch_binaries/raw/main/launch_binary_linux -o launch_binary_linux
 chmod +x launch_binary_linux
+# Fix ionet
+sudo apt install gpg -y
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker 
+sudo docker run --gpus all nvidia/cuda:11.0.3-base-ubuntu18.04 nvidia-smi
+sudo bash -c 'cat <<EOF > /etc/docker/daemon.json
+{
+   "runtimes": {
+       "nvidia": {
+           "path": "nvidia-container-runtime",
+           "runtimeArgs": []
+       }
+   },
+   "exec-opts": ["native.cgroupdriver=cgroupfs"]
+}
+EOF'
+# Final
 sudo reboot
